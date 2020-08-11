c84c3aec0a81b3f6ba9ebe38fb055e4c0aeefc5a
Chinmay Prakash <iamchinmayp@gmail.com>
2020-08-11 21:36:28 +0530

    Added Build

---
+++ build/precache-manifest.ff4b94da9b5aa214ae3c135dfd39ddfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfad9bcaaf66980785645797b7b5727b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff41de8fbb5612fadbca",
+    "url": "/static/css/main.68b9a9da.chunk.css"
+  },
+  {
+    "revision": "7d139d2e22c3e8ae97c9",
+    "url": "/static/js/2.c20ccb72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20ccb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff41de8fbb5612fadbca",
+    "url": "/static/js/main.808735c3.chunk.js"
+  },
+  {
+    "revision": "ac8c1122d7ea4da904c8",
+    "url": "/static/js/runtime-main.e319463a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
